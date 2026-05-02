ca7e0474f24cd58a24629a5d53d74e2b8fe88ffc
Ruslana <shyshkoruslana08@gmail.com>
2026-05-02 19:37:32 +0300

    add task 1

--- Task1.h
+++ Task1.h
@@ -1,8 +1,25 @@
 #ifndef LABA9_TASK1_H
 #define LABA9_TASK1_H
+#include <list>
+#include <algorithm>
+#include <iostream>
+using namespace std;
 
+bool isOdd(int n) {
+    return n % 2 != 0;
+}
 
-class Task1 {
+void Task1() {
+    list<int> lst;
+    for (int i = 0; i < 20; i++) {
+        lst.push_back(rand() % 100);
+    }
+    cout << "Before:\n";
+    for (int x : lst) cout << x << " ";
+    lst.remove_if(isOdd);
+    cout << "\nAfter:\n";
+    for (int x : lst) cout << x << " ";
+    cout << endl;
 
 };
 
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "Task1.h"
+
+using namespace std;
+int main (){
+    Task1();
+}
