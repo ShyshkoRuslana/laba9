f2ab1acbca1a90ff74d94f9581f06b4f1e4797d5
Ruslana <shyshkoruslana08@gmail.com>
2026-05-02 19:44:41 +0300

    add task 2

--- Task2.h
+++ Task2.h
@@ -1,8 +1,19 @@
 #ifndef LABA9_TASK2_H
 #define LABA9_TASK2_H
+#include <array>
+#include <algorithm>
+#include <iostream>
+using namespace std;
 
+bool compare(string a, string b) {
+    return a.size() < b.size();
+}
 
-class Task2 {
+void Task2(){
+    array<string, 5> names = {"Anna", "Oleksandr", "Ira", "Max", "Kristina"};
+    sort(names.begin(), names.end(), compare);
+    for (auto s : names) cout << s << " ";
+    cout << endl;
 
 };
 
--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include "Task1.h"
+#include "Task2.h"
 
 using namespace std;
 int main (){
     Task1();
+    Task2();
+
 }
