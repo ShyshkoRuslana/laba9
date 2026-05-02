1ee006a8e2e4a7111a3cc901087950c933aa6510
Ruslana <shyshkoruslana08@gmail.com>
2026-05-02 19:47:42 +0300

    add task 3

--- Task3.h
+++ Task3.h
@@ -1,9 +1,22 @@
 #ifndef LABA9_TASK3_H
 #define LABA9_TASK3_H
+#include <deque>
+#include <algorithm>
+#include <iostream>
+using namespace std;
 
-
-class Task3 {
-
+struct xAlir {
+    double rate;
+    xAlir(double r) : rate(r) {}
+    void operator()(double &price) {
+        price += price * rate;
+    }
+};
+void Task3() {
+    deque<double> prices = {10, 20, 30};
+    for_each(prices.begin(), prices.end(), xAlir(0.2));
+    for (double p : prices) cout << p << " ";
+    cout << endl;
 };
 
 
--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include "Task1.h"
 #include "Task2.h"
+#include "Task3.h"
 
 using namespace std;
 int main (){
     Task1();
     Task2();
+    Task3();
+
 
 }
